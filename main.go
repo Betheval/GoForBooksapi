fdc143525f841185d1beb378b17493c2f4d8576f
Betheval <miguelgutp@gmail.com>
2024-06-02 15:45:55 +0200

    updating with mongo connection

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Book struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Title     string             `bson:"title,omitempty"`
+	Author    string             `bson:"author,omitempty"`
+	ISBN      string             `bson:"isbn,omitempty"`
+	Publisher string             `bson:"publisher,omitempty"`
+	Year      int                `bson:"year,omitempty"`
+}
+
+var client *mongo.Client
 
 func main() {
 	r := gin.Default()
@@ -12,19 +33,17 @@ func main() {
 		})
 	})
 
-	r.GET("/books", func(c *gin.Context) {
-		//TODO: return first 100 books
-	})
+	r.GET("/books", getBooks)
 
-	r.GET("/books:id", func(c *gin.Context) {
+	r.GET("/book/id/:id", func(c *gin.Context) {
 		//TODO: return a book by ID
 	})
 
-	r.GET("/books:isbn", func(c *gin.Context) {
+	r.GET("/book/isbn/:isbn", func(c *gin.Context) {
 		//TODO: return a book by ISBN
 	})
 
-	r.DELETE("/books:id", func(c *gin.Context) {
+	r.DELETE("/books/:id", func(c *gin.Context) {
 		//TODO: delete a book by ID
 	})
 
@@ -35,4 +54,37 @@ func main() {
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
-//modify a book
+func getBooks(c *gin.Context) {
+	c.JSON(http.StatusOK, " ")
+}
+
+//MongoDB connection
+
+func connectToMongoDB() *mongo.Client {
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI not set in environment")
+	}
+	clientOptions := options.Client().ApplyURI(mongoURI)
+
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
